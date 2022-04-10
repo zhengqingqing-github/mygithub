815b9f80cbd4000bf278937e20e79d0f4e7948e8
zqq <123456@qq.com>
2022-04-10 14:46:06 +0800

    第一次提交

---
+++ src/main/java/com/example/clouds/CloudsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clouds/CloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
